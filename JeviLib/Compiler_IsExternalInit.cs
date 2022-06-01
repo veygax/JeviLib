d4ac2a4f42b32a4ee7011c25a6d5cfa38db8ee43
extraes <52384576+extraes@users.noreply.github.com>
2022-06-01 16:25:12 -0700

    use record type

---
+++ JeviLib/Compiler_IsExternalInit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Define the type "IsExternalInit" so that the compiler lets me use <c>init</c> properties and record types.
+/// </summary>
+public static class IsExternalInit { }
