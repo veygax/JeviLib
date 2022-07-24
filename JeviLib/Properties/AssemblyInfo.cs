64c91b28df42de642437ecf68b3d1255b6b2e38c
extraes <52384576+extraes@users.noreply.github.com>
2022-07-23 17:16:37 -0700

    2.0.0 Allow the JeviLib patch dynamic assembly to see internals, to get delegates for calling.

--- JeviLib/Properties/AssemblyInfo.cs
+++ JeviLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using MelonLoader;
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle(Jevil.JevilBuildInfo.Name)]
@@ -16,10 +17,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion(Jevil.JevilBuildInfo.Version)]
 [assembly: AssemblyFileVersion(Jevil.JevilBuildInfo.Version)]
 [assembly: NeutralResourcesLanguage("en")]
+[assembly: InternalsVisibleTo(Jevil.Patching.DynTools.dynamicAsmName)]
 [assembly: MelonInfo(typeof(Jevil.JeviLib), Jevil.JevilBuildInfo.Name, Jevil.JevilBuildInfo.Version, Jevil.JevilBuildInfo.Author, Jevil.JevilBuildInfo.DownloadLink)]
 [assembly: MelonPriority(-20925)] // rb rd
 
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonGame("Stress Level Zero", "BONEWORKS")] 
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
