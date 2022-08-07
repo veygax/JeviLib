101b04d52508bc4be4c547f6cb15d30727fb3fb7
extraes <52384576+extraes@users.noreply.github.com>
2022-08-06 22:17:37 -0700

    2.0.2 - Dormant CMaps late unload

--- JeviLib/Patching/CMaps.cs
+++ JeviLib/Patching/CMaps.cs
@@ -1,13 +1,49 @@
-﻿namespace Jevil.Patching;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Jevil.Patching;
 
 /// <summary>
 /// Harmony for custom maps if you don't want to make your own Harmony instance.
 /// </summary>
 public static class CMaps
 {
-    private static HarmonyLib.Harmony _cmapHarmony = new("Jevilib.CustomMapHarmony");
+    private static readonly HarmonyLib.Harmony _cmapHarmony = new("Jevilib.CustomMapHarmony");
     /// <summary>
     /// A harmony instance for custom maps.
     /// </summary>
     public static HarmonyLib.Harmony CMHarmony => _cmapHarmony;
+
+
+    static readonly MethodInfo unloadMethod = typeof(AssetBundle).GetMethod(nameof(AssetBundle.Unload), Const.AllBindingFlags);
+    static FieldInfo mapBundleInfo;
+
+    // DO NOT RUN THIS CODE UNLESS THERE IS EVER A REASON TO DO SO
+    // THAT MEANS: NOT UNTIL SOMEONE PROVES DOING THIS FIXES VIDEOPLAYERS
+    internal static void FixUnload()
+    {
+        mapBundleInfo = Utilities.GetTypeFromString("CustomMaps", "MapLoader")?.GetField("mapBundle");
+        MethodInfo psp = Utilities.GetMethodFromString("CustomMaps", "MapLoader", "PostScenePass");
+        if (psp == null) return;
+        Redirect.FromMethod(psp, PostScenePassHook);
+        Disable.WhenCalledFrom(unloadMethod, psp);
+    }
+
+    static void PostScenePassHook()
+    {
+        JeviLib.Log("CustomMaps PostScenePass redirections was called!");
+        AssetBundle mapBundle = (AssetBundle)mapBundleInfo.GetValue(null);
+        MelonLoader.MelonCoroutines.Start(CoUnload(mapBundle));
+    }
+
+    static IEnumerator CoUnload(AssetBundle toUnload)
+    {
+        yield return null;
+        yield return null;
+        toUnload.Unload(false);
+    }
 }
