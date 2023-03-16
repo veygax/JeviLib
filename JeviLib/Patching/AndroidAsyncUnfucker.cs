0203716ba062213053a21b71de959837418a6e19
extraes <52384576+extraes@users.noreply.github.com>
2023-03-16 13:24:44 -0700

    Fix asynchronous code on Quest Monodroid calls into the Java android Looper to get a sync ctx, but uses a method defined in an assembly that exists only in Xamarin.Android, which LL doesnt have imported.

---
+++ JeviLib/Patching/AndroidAsyncUnfucker.cs
@@ -0,0 +1,39 @@
+﻿using Jevil.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jevil.Patching;
+
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+// FUCK OCULUS QUEST
+
+internal static class AndroidAsyncUnfucker
+{
+    internal static void Init()
+    {
+        Redirect.FromMethod(typeof(SynchronizationContext).GetMethod("GetThreadLocalContext", Const.AllBindingFlags), FuckYourContext, true);
+    }
+
+    static SynchronizationContext FuckYourContext()
+    {
+        return null; // go fuck yourself
+    }
+}
