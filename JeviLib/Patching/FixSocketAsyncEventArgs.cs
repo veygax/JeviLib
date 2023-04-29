e92b5912de85679e5948f8308adb4c5bbcdcba6c
extraes <52384576+extraes@users.noreply.github.com>
2023-04-29 12:40:01 -0700

    Clarify some stuff in the SAEA.OC patch. Also lay groundwork for fixing PlayerPrefsException uses. todo: ppe fixes lol

--- JeviLib/Patching/FixSocketAsyncEventArgs.cs
+++ JeviLib/Patching/FixSocketAsyncEventArgs.cs
@@ -14,7 +14,8 @@ internal static class FixSocketAsyncEventArgs
     public static bool Prefix()
     {
 #if DEBUG
-        JeviLib.Log($"Caught call to {nameof(SocketAsyncEventArgs)}.OnCompleted. On Quest this call will hang the game, so it is patched out.");
+        JeviLib.Log($"Caught call to SocketAsyncEventArgs.OnCompleted. On Quest this call will hang the game, so it is patched out on that platform.");
+        JeviLib.Log($"If you are awaiting an HttpClient or WebClient call, or GetAwaiter().GetResult()'ing it, you may find that it never completes on Quest. Please use UnityWebRequests instead.");
 #endif
         // LemonLoader hangs on startup if this isn't patched out. IDK why.
         // For full trace when hang occurs, see: https://cdn.discordapp.com/attachments/716834164762083374/1053981933232738335/image.png
---
+++ JeviLib/Patching/PlayerPrefsExceptionUses.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Jevil.Patching;
+
+internal static class PlayerPrefsExceptionUses
+{
+    // PlayerPrefsException was stripped, so Unhollower trying to instantiate (and throw) them throws a MissingMethodException.
+    // Reimplement functionality of affected methods.
+    public static void Init()
+    {
+        // todo: crashes game on startup. find out why/try to find workaround.
+        // optionally just harmony transpile the managed interop to throw a normal exception
+        return;
+        Redirect.FromDelegate(PlayerPrefs.SetInt, SetIntButBetter, true);
+        Redirect.FromDelegate(PlayerPrefs.SetFloat, SetFloatButBetter, true);
+        Redirect.FromDelegate(PlayerPrefs.SetString, SetStringButBetter, true);
+    }
+
+    static void SetIntButBetter(string key, int value)
+    {
+        if (!PlayerPrefs.TrySetInt(key, value))
+        {
+            throw new Exception("PlayerPrefsException: Could not store preference value");
+        }
+    }
+
+    static void SetFloatButBetter(string key, float value)
+    {
+        if (!PlayerPrefs.TrySetFloat(key, value))
+        {
+            throw new Exception("PlayerPrefsException: Could not store preference value");
+        }
+    }
+
+    static void SetStringButBetter(string key, string value)
+    {
+        if (!PlayerPrefs.TrySetSetString(key, value))
+        {
+            throw new Exception("PlayerPrefsException: Could not store preference value");
+        }
+    }
+}
